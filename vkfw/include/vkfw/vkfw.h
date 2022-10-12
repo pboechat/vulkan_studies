cfd7a9a2a125c5c3ec534541435fbefa76c09aa3
pboechat <pboechat@gmail.com>
2022-10-12 08:31:59 +0100

    Refactored vkfw to header-only. Added vkfwArraySize() macro.

--- vkfw/include/vkfw/vkfw.h
+++ vkfw/include/vkfw/vkfw.h
@@ -23,6 +23,8 @@
 #endif
 
 #include <cstdint>
+#include <fstream>
+#include <vector>
 #include <string>
 
 namespace vkfw
@@ -47,11 +49,78 @@ namespace vkfw
 		}
 	};
 
-	extern const Result sc_success;
+	inline void fail(const char *msg)
+	{
+#if defined _WIN32 || defined _WIN64
+		MessageBox(nullptr, msg, "FAILURE", MB_ICONERROR | MB_OK);
+#else
+		printf("%s", msg);
+#endif
+		exit(-1);
+	}
 
-	void fail(const char *msg);
-	void warn(const char *msg);
-	const char *getVkErrorString(VkResult errorCode);
+	inline void warn(const char *msg)
+	{
+#if defined _WIN32 || defined _WIN64
+		MessageBox(nullptr, msg, "WARNING", MB_ICONWARNING | MB_OK);
+#else
+		printf("%s", msg);
+#endif
+	}
+
+	inline const char *getVkErrorString(VkResult errorCode)
+	{
+		switch (errorCode)
+		{
+#define STR(r)   \
+	case VK_##r: \
+		return #r
+			STR(NOT_READY);
+			STR(TIMEOUT);
+			STR(EVENT_SET);
+			STR(EVENT_RESET);
+			STR(INCOMPLETE);
+			STR(ERROR_OUT_OF_HOST_MEMORY);
+			STR(ERROR_OUT_OF_DEVICE_MEMORY);
+			STR(ERROR_INITIALIZATION_FAILED);
+			STR(ERROR_DEVICE_LOST);
+			STR(ERROR_MEMORY_MAP_FAILED);
+			STR(ERROR_LAYER_NOT_PRESENT);
+			STR(ERROR_EXTENSION_NOT_PRESENT);
+			STR(ERROR_FEATURE_NOT_PRESENT);
+			STR(ERROR_INCOMPATIBLE_DRIVER);
+			STR(ERROR_TOO_MANY_OBJECTS);
+			STR(ERROR_FORMAT_NOT_SUPPORTED);
+			STR(ERROR_SURFACE_LOST_KHR);
+			STR(ERROR_NATIVE_WINDOW_IN_USE_KHR);
+			STR(SUBOPTIMAL_KHR);
+			STR(ERROR_OUT_OF_DATE_KHR);
+			STR(ERROR_INCOMPATIBLE_DISPLAY_KHR);
+			STR(ERROR_VALIDATION_FAILED_EXT);
+			STR(ERROR_INVALID_SHADER_NV);
+#undef STR
+		default:
+			return "UNKNOWN_ERROR";
+		}
+	}
+
+	inline std::vector<char> readFile(const std::string &filename)
+	{
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+		if (!file.is_open())
+		{
+			return {};
+		}
+
+		auto bufferSize = file.tellg();
+		std::vector<char> buffer(bufferSize);
+		file.seekg(0);
+		file.read(buffer.data(), bufferSize);
+		file.close();
+
+		return buffer;
+	}
 
 }
 
@@ -81,5 +150,7 @@ namespace vkfw
 			vkfw::fail(vkfw::getVkErrorString(__vkResult)); \
 		}                                                   \
 	}
+
+#define vkfwArraySize(arr) sizeof(arr) / sizeof(arr[0])
 
 #endif
